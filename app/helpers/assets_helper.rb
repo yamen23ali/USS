3c3d8a14f39879710a2379d9ca3cdd8f8a353e2b
Yamen Ali <yamen.ali@zalando-lounge.de>
2016-01-03 18:03:05 +0100

    Add assets table

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -17,6 +17,19 @@ module AssetsHelper
   	SubCategory.find(@asset.sub_category_id).name unless @asset.sub_category_id.nil?
   end
 
+  def descriptors
+    @asset.descriptors.map { |descriptor|
+
+      "#{Descriptor.find(descriptor.descriptor_id).name} : #{descriptor.descriptor_value||=""}"
+    }
+  end
+
+  def tags
+    @asset.tags.map{ |tag| 
+      "##{tag.name}"
+    }.join(' ')
+  end
+
   def asset_preview
   	asset_photos = @asset.asset_data.map {|data| data.photo if !data.photo.nil? }
   	
@@ -29,31 +42,30 @@ module AssetsHelper
   end
 
   def existing_photos( photo_index, f )
-
     f.fields_for :asset_data , @asset.photos[ photo_index ] do |builder|
       render 'asset_photos_form', :f => builder
     end
-    
   end
 
   def missing_photo_block( f )
-
-      f.fields_for :asset_data , @asset.asset_data.build do |builder|
-        render 'asset_photos_form', :f => builder
-      end
+    f.fields_for :asset_data , @asset.asset_data.build do |builder|
+      render 'asset_photos_form', :f => builder
+    end
   end
 
   def icon(type)
-    
     case type
       when "plus"
         "<span class=\"glyphicon glyphicon-plus\"></span>".html_safe
       when "remove"
         "<span class=\"glyphicon glyphicon-remove\"></span>".html_safe
+      when "edit"
+        "<span class=\"glyphicon glyphicon-pencil\"></span>".html_safe
+      when "show"
+        "<span class=\"glyphicon glyphicon-eye-open\"></span>".html_safe
       else
         "Click Me"
     end
-
   end
 
 end
