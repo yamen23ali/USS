dc33bf1d7c4ee2384ec87b96614c20af5e3d7d09
Yamen Ali <yamen.ali@zalando-lounge.de>
2016-02-21 18:41:04 +0100

    Offer show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def icon(type)
+    case type
+      when "plus"
+        "<span class=\"glyphicon glyphicon-plus\"></span>".html_safe
+      when "remove"
+        "<span class=\"glyphicon glyphicon-remove\"></span>".html_safe
+      when "edit"
+        "<span class=\"glyphicon glyphicon-pencil\"></span>".html_safe
+      when "show"
+        "<span class=\"glyphicon glyphicon-eye-open\"></span>".html_safe
+      else
+        "Click Me"
+    end
+  end
+
 end
--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -50,19 +50,4 @@ module AssetsHelper
     end
   end
 
-  def icon(type)
-    case type
-      when "plus"
-        "<span class=\"glyphicon glyphicon-plus\"></span>".html_safe
-      when "remove"
-        "<span class=\"glyphicon glyphicon-remove\"></span>".html_safe
-      when "edit"
-        "<span class=\"glyphicon glyphicon-pencil\"></span>".html_safe
-      when "show"
-        "<span class=\"glyphicon glyphicon-eye-open\"></span>".html_safe
-      else
-        "Click Me"
-    end
-  end
-
 end
